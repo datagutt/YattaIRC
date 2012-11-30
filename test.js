6affbe9c210cdcea4f316113a679346830ab665b
datagutt <datagutt666@hotmail.com>
2012-11-30 19:23:22 +0100

    meh

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ YattaIRC = require('./lib/IRC');
 IRC = new YattaIRC;
 session = IRC.connect('irc.freenode.net', 6667);
 IRC.nick(session, 'YattaBot');
-IRC.user(session, 'yatta', 8, 'Hiro Nakamura');
+IRC.user(session, 'yatta', 1, 'Hiro Nakamura');
 IRC.join(session, '#blamesamsung');
 
 // Command line interface
