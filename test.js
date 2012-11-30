f879a7a1c40aeee8e900fe69d38d96cec5f8a56b
datagutt <datagutt666@hotmail.com>
2012-11-30 13:37:55 +0100

    Command line

--- test.js
+++ test.js
@@ -4,3 +4,14 @@ session = IRC.connect('irc.freenode.net', 6667);
 IRC.nick(session, 'YattaBot');
 IRC.user(session, 'yatta', 8, 'Hiro Nakamura');
 IRC.join(session, '#blamesamsung');
+
+// Command line interface
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(msg){
+	try{
+		IRC.raw(session, msg);
+	}catch(e){
+		console.log(e);
+	}
+});
